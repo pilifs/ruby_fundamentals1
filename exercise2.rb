78d7311d3e160f399c252641aa7e5402af1e7f87
Filip Stankovic <filips@gmail.com>
2016-01-12 14:55:53 -0500

    Adding second question

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,11 @@
 # Multiply the cost of the meal by 18% for a generous tips
 # Need to round to 2 digits due to float imprecission, takes more than 53 bits to calculate answer and gives 63.249999999*
-puts "Tipping generously (18%) for a $55 meal brings your total to $#{(55*1.15).round(2()}"
+puts "Tipping generously (18%) for a $55 meal brings your total to $#{(55*1.15).round(2)}"
+
+# Convert string to integer and add it
+
+puts "Enter first number"
+value_one = gets.chomp.to_i
+puts "Enter second number"
+value_two = gets.chomp.to_i
+puts "Your two numbers added together equal #{value_one + value_two}"
