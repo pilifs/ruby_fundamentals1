ffd1f11fc83d9e8bfe0e043c6d875b8d9e8a25bd
Filip Stankovic <filips@gmail.com>
2016-01-12 15:55:59 -0500

    Adding exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,13 @@
+# Get user age
+# Sanitize input and only allow user to proceed if age between 1-120
+
 puts "What is your name?"
 name = gets.chomp
-puts name
+puts "Hi #{name}!"
+puts "How old are you?"
+age = 0
+while age < 1 || age > 120
+  age = gets.chomp.to_i
+end
+birthyear = 2016 - age
+puts "You were born in #{birthyear} or #{birthyear + 1}"
